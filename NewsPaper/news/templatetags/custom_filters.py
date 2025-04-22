c1f0dae825de013fcbc98de4c528c30fc75a2349
Kazikhanov_2005 <kazikhanov345@gmail.com>
2025-04-22 16:42:01 +0300

    Исправил баг

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -5,6 +5,7 @@ register = template.Library()
 DELETE_WORDS = {1:'редиска', 2:'огурец'
                 }
 
+
 def _search(list, item):
     if list.get(item):
         return list.get(item)
@@ -13,9 +14,9 @@ def _search(list, item):
 
 @register.filter()
 def censor(content):
-    content = content.lower()
+
     for i in range(1, len(DELETE_WORDS) + 1):
-        if _search(DELETE_WORDS, i) in content:
+        if _search(DELETE_WORDS, i) in content.lower():
             word = _search(DELETE_WORDS, i)
             edit_word = _search(DELETE_WORDS, i)[0] + (len(word) - 1) * '*'
             content = content.replace(word, edit_word)
